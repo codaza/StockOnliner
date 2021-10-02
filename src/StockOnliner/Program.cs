bc3ab109836bbeeac117668053890cfb17b64a8c
codaza <84292305+codaza@users.noreply.github.com>
2021-10-02 14:33:52 +0300

    Initial commit

---
+++ src/StockOnliner/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StockOnliner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StockExchangeMonitor stockExchangeMonitor = new StockExchangeMonitor();
+            stockExchangeMonitor.PriceChangeHandler = ShowPrice;
+            stockExchangeMonitor.Start();
+        }
+
+        public static void ShowPrice(int price)
+        {
+            Console.WriteLine($"New price is: {price}");
+        }
+    }
+}
---
+++ src/StockOnliner/StockExchangeMonitor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace StockOnliner
+{
+    public class StockExchangeMonitor
+    {
+        public delegate void PriceChange(int price);
+
+        public PriceChange PriceChangeHandler { get; set; }
+
+        public void Start()
+        {
+            while (true)
+            {
+                int bankOfAmericaPrice = (new Random()).Next(100);
+
+                PriceChangeHandler(bankOfAmericaPrice);
+
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
